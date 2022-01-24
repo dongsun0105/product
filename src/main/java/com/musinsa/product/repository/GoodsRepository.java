628bbd0b0a0397b44bc31bb15fe90314b9932bc4
dongsun0105 <ecle0105@gmail.com>
2022-01-25 01:22:36 +0900

    상속 Repository 인터페이스 수정

--- src/main/java/com/musinsa/product/repository/GoodsRepository.java
+++ src/main/java/com/musinsa/product/repository/GoodsRepository.java
@@ -1,7 +1,7 @@
 package com.musinsa.product.repository;
 
 import com.musinsa.product.entity.Goods;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GoodsRepository extends PagingAndSortingRepository<Goods, Integer> {
+public interface GoodsRepository extends JpaRepository<Goods, Integer> {
 }
